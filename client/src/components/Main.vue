8043e33687361f15e7665351976c908392ec1ea0
theoigen <oigenZ@icloud.com>
2019-06-19 12:14:22 +0300

    project started

---
+++ client/src/components/Main.vue
@@ -0,0 +1,98 @@
+<template>
+  <div>
+    <b-carousel
+      id="carousel-1"
+      v-model="slide"
+      :interval="4000"
+      controls
+      indicators
+      background="#ababab"
+      img-width="1024"
+      img-height="480"
+      style="text-shadow: 1px 1px 2px #333;"
+      @sliding-start="onSlideStart"
+      @sliding-end="onSlideEnd"
+    >
+      <!-- Text slides with image -->
+
+      <b-carousel-slide v-for="film in films" :key="film.id" :caption="film.name">
+        <img slot="img" class="d-block img-fluid w-100" width="1024" height="300" :src="film.img">
+      </b-carousel-slide>
+
+      <b-carousel-slide
+        caption="First slide"
+        text="Nulla vitae elit libero, a pharetra augue mollis interdum."
+        img-src="https://picsum.photos/1024/480/?image=52"
+      ></b-carousel-slide>
+
+      <!-- Slides with custom text -->
+      <b-carousel-slide img-src="https://picsum.photos/1024/480/?image=54">
+        <h1>Hello world!</h1>
+      </b-carousel-slide>
+
+      <!-- Slides with image only -->
+      <b-carousel-slide img-src="https://picsum.photos/1024/480/?image=58"></b-carousel-slide>
+
+      <!-- Slides with img slot -->
+      <!-- Note the classes .d-block and .img-fluid to prevent browser default image alignment -->
+      <b-carousel-slide>
+        <img
+          slot="img"
+          class="d-block img-fluid w-100"
+          width="1024"
+          height="480"
+          src="https://picsum.photos/1024/480/?image=55"
+          alt="image slot"
+        >
+      </b-carousel-slide>
+
+      <!-- Slide with blank fluid image to maintain slide aspect ratio -->
+      <b-carousel-slide caption="Blank Image" img-blank img-alt="Blank image">
+        <router-link to="/about">Watch this!)</router-link>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse eros felis, tincidunt
+          a tincidunt eget, convallis vel est. Ut pellentesque ut lacus vel interdum.
+        </p>
+      </b-carousel-slide>
+    </b-carousel>
+
+    <p class="mt-4">
+      Slide #: {{ slide }}
+      <br>
+      Sliding: {{ sliding }}
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      slide: 0,
+      sliding: null,
+      films: [
+        {
+          id: 0,
+          name: 'Bladerunner',
+          img:
+            'http://cinefex.com/blog/wp-content/uploads/2015/03/Blade-Runner-Spinner-1.jpg'
+        },
+        {
+          id: 1,
+          name: 'La-La-Land',
+          img:
+            'https://www.dailydot.com/wp-content/uploads/daf/ff/4d7dc314ab1b01881879b26700fc292d.jpg'
+        }
+      ]
+    }
+  },
+  methods: {
+    onSlideStart(slide) {
+      this.sliding = true
+    },
+    onSlideEnd(slide) {
+      this.sliding = false
+    }
+  }
+}
+</script>
---
+++ client/src/components/Navbar.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="mb-5">
+    <b-navbar toggleable="lg" type="dark" variant="info" height="200" fixed="top">
+      <b-navbar-brand>16:9</b-navbar-brand>
+
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+      <b-collapse id="nav-collapse" is-nav>
+        <b-navbar-nav class="ml-auto">
+          <b-nav-item to="/">Home</b-nav-item>
+          <b-nav-item to="/about">About</b-nav-item>
+          <b-nav-item-dropdown right text="User">
+            <b-dropdown-item>Profile</b-dropdown-item>
+            <b-dropdown-item>Logout</b-dropdown-item>
+          </b-nav-item-dropdown>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
+  </div>
+</template>
+
+<style scoped lang="scss">
+a.nav-link {
+  text-decoration: none;
+  color: white;
+  &.router-link-exact-active {
+    color: white;
+  }
+  :focus {
+    color: white;
+  }
+}
+</style>
